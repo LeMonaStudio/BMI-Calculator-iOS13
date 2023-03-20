5ff16a29489ab55af93fc9fb2a3bcfb979264820
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2023-03-20 18:02:07 +0100

    Created a BMI struct to hold the BMI value, the advice for the user and a specific UIColor to display in the UI in cases where the user is underweight, normal weight or overweight.

---
+++ BMI Calculator/Model/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Musa Lawrence on 3/20/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
